26264c83871890f894d28158fca10539f8ac9530
Lizzy <elizabethstrydom.mail@gmail.com>
2022-07-13 10:10:34 +0200

    First Commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import classes from './App.module.css';
+import ProductData from './ProductData';
+import ProductPreview from './ProductPreview';
+import ProductDetails from './ProductDetails';
+import Topbar from './Topbar';
+
+class App extends Component {
+  state = {
+    productData: ProductData
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Topbar />
+
+        <div className={classes.MainContainer}>
+          <div className={classes.ProductPreview}>
+            <ProductPreview />
+          </div>
+
+          <div className={classes.ProductData}>
+            <ProductDetails data={this.state.productData} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
